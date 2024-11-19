1cead004ff5adddd59792eb9e882c054189fa012
AndrewOwotomo <Andrewo78@nycstudents.net>
2024-11-19 09:44:35 -0500

    w

---
+++ SHEWANNAPLAYWITCARTI.py
@@ -0,0 +1,10 @@
+""" So um ima like cartinese """
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("My First Tkinter App")
+
+# Create a label
+label = tk.Label(window, text="Hello, Tkinter!")
+label.pack()
